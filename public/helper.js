9724e8e992ada2179988c6e7135ff39db02d4614
frankcash <cfcash@mtu.edu>
2014-04-12 00:15:33 -0500

    no need to display the objects multiple times

--- public/helper.js
+++ public/helper.js
@@ -1,6 +1,6 @@
 $.getJSON('/scrape', function(data){ // gets the JSON info
   console.log(data);
-  for(var i = 0; i<2;i++){
+  for(var i = 0; i<1;i++){
     $("<p><b>" + data + ".</b></p>").appendTo("#helper")
   }
 });
