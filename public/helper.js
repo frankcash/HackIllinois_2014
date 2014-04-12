ee8dbddb2a0de89a65d48e653023672d4d69c49f
frankcash <cfcash@mtu.edu>
2014-04-12 02:15:06 -0500

    PROGRESS HAS BEEN MADE. HAIL JOHN NOVAK.

--- public/helper.js
+++ public/helper.js
@@ -1,7 +1,7 @@
 $.getJSON('/scrape', function(data){ // gets the JSON info
   console.log(data);
-  for(var i = 0; i<1;i++){
-    $("<p><b>" + data + ".</b></p>").appendTo("#helper")
+  for(var i = 0; i<5;i++){
+    $("<p><b>" + data[i].price + ".</b></p>").appendTo("#helper")
   }
 });
 $( document ).ready(function() {
