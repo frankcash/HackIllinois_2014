146f77ab9ca58063340df5c6c7641dee6c3b6cdb
frankcash <cfcash@mtu.edu>
2014-04-11 23:30:40 -0500

    removed unnecessary code

--- public/helper.js
+++ public/helper.js
@@ -1,13 +1,10 @@
 $.getJSON('/scrape', function(data){ // gets the JSON info
-  // console.log(data);
+  console.log(data);
   for(var i = 0; i<2;i++){
-    $("<p><b>" + data[i]".</b></p>").appendTo("#helper")
+    $("<p><b>" + data + ".</b></p>").appendTo("#helper")
   }
 });
 $( document ).ready(function() {
-  // console.log( "ready!" );
-  // $("b").mouseover(function(){
-  //  $("<p>" + data[i].rank + "</p>")
-  // });
+
 
 });
